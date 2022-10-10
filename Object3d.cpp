0c474b3da542a10a1f9807f59e810b51d830cbcf
yamashitatubasa <k021g1247@g.neec.ac.jp>
2022-10-10 23:08:40 +0900

    01_03

--- Object3d.cpp
+++ Object3d.cpp
@@ -31,7 +31,8 @@ XMFLOAT3 Object3d::up = { 0, 1, 0 };
 D3D12_VERTEX_BUFFER_VIEW Object3d::vbView{};
 D3D12_INDEX_BUFFER_VIEW Object3d::ibView{};
 Object3d::VertexPosNormalUv Object3d::vertices[vertexCount];
-unsigned short Object3d::indices[planeCount * 3];
+//unsigned short Object3d::indices[planeCount * 3];
+unsigned short Object3d::indices[indexCount];
 
 void Object3d::StaticInitialize(ID3D12Device * device, int window_width, int window_height)
 {
@@ -126,6 +127,17 @@ void Object3d::CameraMoveVector(XMFLOAT3 move)
 
 	SetEye(eye_moved);
 	SetTarget(target_moved);
+}
+
+void Object3d::CameraMoveEyeVector(XMFLOAT3 move) 
+{
+	XMFLOAT3 eye_moved = GetEye();
+
+	eye_moved.x += move.x;
+	eye_moved.y += move.y;
+	eye_moved.z += move.z;
+
+	SetEye(eye_moved);
 }
 
 void Object3d::InitializeDescriptorHeap()
@@ -389,7 +401,7 @@ void Object3d::LoadTexture()
 void Object3d::CreateModel()
 {
 	HRESULT result = S_FALSE;
-
+	/*
 	std::vector<VertexPosNormalUv> realVertices;
 	// 頂点座標の計算（重複あり）
 	{
@@ -508,6 +520,27 @@ void Object3d::CreateModel()
 		XMStoreFloat3(&vertices[index1].normal, normal);
 		XMStoreFloat3(&vertices[index2].normal, normal);
 	}
+	*/
+
+	// 四角形の頂点データ
+	VertexPosNormalUv verticesSquare[] = {
+		{{-5.0f,-5.0f,0.0f},{0,0,1},{0,1}},// 左下
+		{{-5.0f,+5.0f,0.0f},{0,0,1},{0,1}},// 左上
+		{{+5.0f,-5.0f,0.0f},{0,0,1},{1,1}},// 右下
+		{{+5.0f,+5.0f,0.0f},{0,0,1},{1,0}},// 右上
+	};
+	
+	// メンバ変数にコピー
+	std::copy(std::begin(verticesSquare), std::end(verticesSquare), vertices);
+
+	// 四角形のインデックスデータ
+	unsigned short indicesSquare[] = {
+		0,1,2,// 三角形1
+		2,1,3,// 三角形2
+	};
+
+	// メンバ変数にコピー
+	std::copy(std::begin(indicesSquare), std::end(indicesSquare), indices);
 
 	UINT sizeVB = static_cast<UINT>(sizeof(vertices));
 
